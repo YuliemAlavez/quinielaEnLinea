4571a93d5fbb4d2704e78890e0cffbc2ee90d95f
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-01-28 17:20:22 -0600

    Creación de lista de Equipos
    Creación de la página que lista los equipos que están en la base de
    datos.

--- symfony/src/Quiniela/MainBundle/Controller/DefaultController.php
+++ symfony/src/Quiniela/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace Quiniela\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\Mapping as ORM;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('QuinielaMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function allTeamsAction(){
+    	$em= $this->getDoctrine()->getEntityManager();
+        
+        $teams= $em->getRepository('QuinielaMainBundle:Team')->findAll();            	
+
+
+    	return $this->render(
+    		'QuinielaMainBundle:Default:viewAllTeams.html.twig',
+    		array('teams'=>$teams)
+    	);
+    	
     }
 }
